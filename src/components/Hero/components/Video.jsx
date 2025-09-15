43c511b70a8760fa37e0bb1b30dea975a87ee7e9
Davi Ribeiro <davi.75.ef@gmail.com>
2025-09-15 13:48:20 -0300

    implement HeroSection

---
+++ src/components/Hero/components/Video.jsx
@@ -0,0 +1,46 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+const Video = () => {
+  const videoRef = useRef()
+
+  const isMobile = useMediaQuery({ maxWidth: 767   })
+
+  useGSAP(() => {
+    const startValue = isMobile ? 'top 50%' : 'center 60%'
+    const endValue = isMobile ? '120% top' : 'bottom top'
+
+    const videoTl = gsap.timeline({
+      scrollTrigger:{
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      videoTl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      })
+    }
+  },[])
+
+
+  return (
+    <div className="video absolute inset-0 -z-10">
+      <video
+        ref={videoRef}
+        src="/videos/output.mp4"
+        muted
+        playsInline
+        preload='auto'
+      />
+    </div>
+  )
+}
+
+export default Video
